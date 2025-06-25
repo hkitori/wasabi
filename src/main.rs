bd43c2007979e9ca1d8aa590ff69863822047c0f
hkitori <hkitori@users.noreply.github.com>
2025-06-25 19:27:24 +0900

    p231: page fault exception happnes at reading address 0

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,12 @@ use wasabi::uefi::EfiSystemTable;
 //use wasabi::uefi::MemoryMapHolder;
 use wasabi::uefi::VramTextWriter;
 use wasabi::warn;
+use wasabi::x86::flush_tlb;
 use wasabi::x86::hlt;
 use wasabi::x86::init_exceptions;
+use wasabi::x86::read_cr3;
 use wasabi::x86::trigger_debug_interrupt;
+use wasabi::x86::PageAttr;
 
 // UEFI が直接呼び出すUEFI アプリケーションのエントリポイント
 #[no_mangle]
@@ -87,6 +90,20 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 
     info!("Reading from memory address...");
 
+    #[allow(clippy::zero_ptr)]
+    #[allow(deref_nullptr)]
+    let value_at_zero = unsafe { *(0 as *const u8) };
+    info!("value_at_zero = {value_at_zero}");
+
+    let page_table = read_cr3();
+    unsafe {
+        (*page_table)
+            .create_mapping(0, 4096, 0, PageAttr::NotPresent)
+            .expect("Failed to unmap page 0");
+    }
+    flush_tlb();
+
+    info!("Reading from memory address 0... (again)");
     #[allow(clippy::zero_ptr)]
     #[allow(deref_nullptr)]
     let value_at_zero = unsafe { *(0 as *const u8) };
