c3590008fd6a545b1e6c36e6cde347d40a1e6746
hkitori <hkitori@users.noreply.github.com>
2025-06-29 18:20:43 +0900

    p292: remove paging demo and refactoring paging initializaion

--- src/main.rs
+++ src/main.rs
@@ -29,16 +29,12 @@ use wasabi::qemu::QemuExitCode;
 use wasabi::uefi::init_vram;
 use wasabi::uefi::locate_loaded_image_protocol;
 use wasabi::uefi::EfiHandle;
-use wasabi::uefi::EfiMemoryType;
+//use wasabi::uefi::EfiMemoryType;
 use wasabi::uefi::EfiSystemTable;
 //use wasabi::uefi::MemoryMapHolder;
 use wasabi::uefi::VramTextWriter;
 use wasabi::warn;
-use wasabi::x86::flush_tlb;
 use wasabi::x86::init_exceptions;
-use wasabi::x86::read_cr3;
-use wasabi::x86::trigger_debug_interrupt;
-use wasabi::x86::PageAttr;
 
 // UEFI が直接呼び出すUEFI アプリケーションのエントリポイント
 #[no_mangle]
@@ -65,33 +61,9 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let memory_map = init_basic_runtime(image_handle, efi_system_table);
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
     init_allocator(&memory_map);
-    let cr3 = wasabi::x86::read_cr3();
-    println!("cr3 = {cr3:#p}");
-    let t = Some(unsafe { &*cr3 });
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
 
     let (_gdt, _idt) = init_exceptions();
-    info!("Exception initialized!");
-    trigger_debug_interrupt();
-    info!("Execution continued.");
     init_paging(&memory_map);
-    info!("Now we are using our own page tables!");
-
-    // Unmap page 0 to detect null ptr dereference
-    let page_table = read_cr3();
-    unsafe {
-        (*page_table)
-            .create_mapping(0, 4096, 0, PageAttr::NotPresent)
-            .expect("Failed to unmap page 0");
-    }
-    flush_tlb();
-
     init_hpet(acpi);
     let t0 = global_timestamp();
     let task1 = Task::new(async move {
